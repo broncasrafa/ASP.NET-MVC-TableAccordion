e12d65f8d0a5eac7228f24892f9702ad1b55891c
Rafael Francisco <broncasrafa@gmail.com>
2019-11-02 20:37:02 -0300

    Initial

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            try
+            {
+                List<Parcela> Parcelas = Parcela.GetParcelas();
+
+                return Json(new { data = Parcelas, status = "OK" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensagem = $"Ocorreu um erro: {ex.Message}", status = "ERROR" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult SalvarParcela(Parcela parcela, int idPagamento, int idProjeto)
+        {
+            try
+            {
+                if (idPagamento > 0)
+                {                                        
+                    return Json(new { data = parcela, status = "OK" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    // cria o pagamento no projeto
+                    // pega o id do pagamento criado e salva a parcela
+                    return Json(new { data = parcela, status = "OK" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensagem = $"Ocorreu um erro: {ex.Message}", status = "ERROR" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult EditarParcela(Parcela parcela, int idPagamento, int idProjeto)
+        {
+            try
+            {
+                if (idPagamento > 0)
+                {                    
+                    return Json(new { data = parcela, status = "OK" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    // cria o pagamento no projeto
+                    // pega o id do pagamento criado e salva a parcela
+                    return Json(new { data = parcela, status = "OK" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensagem = $"Ocorreu um erro: {ex.Message}", status = "ERROR" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult DeletarParcela(int idParcela)
+        {
+            try
+            {
+                return Json(new { mensagem = "Parcela removida com sucesso", status = "OK" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensagem = $"Ocorreu um erro: {ex.Message}", status = "ERROR" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ WebApplication/Models/Parcela.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication.Models
+{
+    public class Parcela
+    {
+        public int Id { get; set; }
+        public string AreaOrcada { get; set; }
+        public string EmpresaPagadora { get; set; }
+        public decimal Valor { get; set; }
+        public string NumeroPedido { get; set; }
+        public DateTime DataPagamento { get; set; }
+
+        public static List<Parcela> GetParcelas()
+        {
+            List<Parcela> Parcelas = new List<Parcela>();
+            Parcelas.Add(new Parcela { Id = 1, AreaOrcada = "Segurança", EmpresaPagadora = "Empresa 1", Valor = 10000M, NumeroPedido = "012345", DataPagamento = new DateTime(2019, 6, 1) });
+            Parcelas.Add(new Parcela { Id = 2, AreaOrcada = "Comunicação", EmpresaPagadora = "Empresa 2", Valor = 20000M, NumeroPedido = "678901", DataPagamento = new DateTime(2019, 7, 1) });
+            Parcelas.Add(new Parcela { Id = 3, AreaOrcada = "Publicidade", EmpresaPagadora = "Empresa 3", Valor = 30000M, NumeroPedido = "234567", DataPagamento = new DateTime(2019, 8, 1) });
+            Parcelas.Add(new Parcela { Id = 4, AreaOrcada = "Esporte", EmpresaPagadora = "Empresa 4", Valor = 40000M, NumeroPedido = "890123", DataPagamento = new DateTime(2019, 9, 1) });
+            Parcelas.Add(new Parcela { Id = 5, AreaOrcada = "Financeiro", EmpresaPagadora = "Empresa 5", Valor = 50000M, NumeroPedido = "456789", DataPagamento = new DateTime(2019, 10, 1) });
+
+            return Parcelas;
+        }
+    }
+}
